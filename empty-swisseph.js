8e6113d9f4dfb1dbad296fd9d5f16ac492055b1f
SuvijjanoS <sinthu.satawiriya@gmail.com>
2025-05-16 13:39:22 +0700

    Update empty-swisseph.js

--- empty-swisseph.js
+++ empty-swisseph.js
@@ -1,10 +1,16 @@
-// empty-swisseph.js
-module.exports = {
-  SE_GREG_CAL: 1,
-  swe_julday: () => { throw new Error('Use WASM, not native swisseph'); },
-  swe_calc_ut: () => { throw new Error('Use WASM'); },
-  swe_set_ephe_path: () => {},
-  swe_houses: () => {},
+// Empty module to prevent swisseph compilation
+const emptyModule = {
+  swe_julday: () => 0,
+  swe_calc_ut: () => 0,
   swe_revjul: () => {},
-  swe_close: () => {}
+  swe_close: () => {},
+  swe_set_ephe_path: () => {},
+  SE_GREG_CAL: 1
 };
+
+// Support both CommonJS and ES Module imports
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = emptyModule;
+}
+
+export default emptyModule;
